c5fff30615508c7390b0c044f8d1fb9651f8462f
moisotico <moiso.camposcr@gmail.com>
2019-03-28 08:08:00 -0600

    Functioning 1:2 demux, tester, and testbench

--- Tarea2/demux/demux.v
+++ Tarea2/demux/demux.v
@@ -5,30 +5,30 @@ module demux(
                     input [3:0] data_in );
 
     //selector (using internal signal)
-    reg s;
+    reg s = 'b0;
+
     //using non-blocking asignation on positive edge    
-    always@(posedge clk) 
-    begin
+    always@(posedge clk) begin
+        s <= ~s; 
         if (reset_L == 0)
         begin
             data_out0 <= 0;
             data_out1 <= 0;
         end
        
-        //reset_L is activated and clocks are on
-        else
+        else                            //when reset_L is activated
         begin
         // demux to data_out0
             if (s==0) 
             begin
             data_out0 <= data_in;
-            data_out1 <=0;
+            data_out1 <= 0;
             end
 
         // demux to data_out1
         else
         begin
-            data_out0 <=0;
+            data_out0 <= 0;
             data_out1 <= data_in;
         end
       end
