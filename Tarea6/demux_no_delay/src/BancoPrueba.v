8a0df2a66c0ca8c4a17822cb6331c63a9868c9a1
moisotico <moiso.camposcr@gmail.com>
2019-05-09 10:15:14 -0600

    Checking the valid bit

--- Tarea6/demux_no_delay/src/BancoPrueba.v
+++ Tarea6/demux_no_delay/src/BancoPrueba.v
@@ -3,33 +3,43 @@
 `include "./src/demux_comb.v"
 `include "./lib/cmos_cells.v"
 
-module TestBench;                               //testbench doesn't have ports
+module TestBench;                               
 
-    /*AUTOWIRE*/
-    // Beginning of automatic wires (for undeclared instantiated-module outputs)
-    wire		clk;			// From probador of probador.v
-    wire [3:0]		data_in;		// From probador of probador.v
-    wire [3:0]		data_out0;		// From dmux of demux.v
-    wire [3:0]		data_out1;		// From dmux of demux.v
-    wire		reset_L;		// From probador of probador.v
-    // End of automatics
-   /*AUTOREGINPUT*/
+   /*AUTOWIRE*/
+   // Beginning of automatic wires (for undeclared instantiated-module outputs)
+   wire			clk;			// From probador of probador.v
+   wire [3:0]		data_in;		// From probador of probador.v
+   wire [3:0]		data_out0;		// From dmux of demux_comb.v
+   wire [3:0]		data_out1;		// From dmux of demux_comb.v
+   wire			reset_L;		// From probador of probador.v
+   wire			valid_in;		// From probador of probador.v
+   wire [1:0]		valid_out;		// From probador of probador.v
+   wire			valid_out0;		// From dmux of demux_comb.v
+   wire			valid_out1;		// From dmux of demux_comb.v
+   // End of automatics
+   /*AUTOREGINPUT*/    
 
-    demux dmux (/*AUTOINST*/
-		// Outputs
-		.data_out0		(data_out0[3:0]),
-		.data_out1		(data_out1[3:0]),
-		// Inputs
-		.clk			(clk),
-		.reset_L		(reset_L),
-		.data_in		(data_in[3:0]));
+   demux_comb dmux (/*AUTOINST*/
+		    // Outputs
+		    .data_out0		(data_out0[3:0]),
+		    .data_out1		(data_out1[3:0]),
+		    .valid_out0		(valid_out0),
+		    .valid_out1		(valid_out1),
+		    // Inputs
+		    .clk		(clk),
+		    .reset_L		(reset_L),
+		    .valid_in		(valid_in),
+		    .data_in		(data_in[3:0]));
+   
 
-    probador probador (/*AUTOINST*/
-		       // Outputs
-		       .clk		(clk),
-		       .reset_L		(reset_L),
-		       .data_in		(data_in[3:0]),
-		       // Inputs
-		       .data_out0	(data_out0[3:0]),
-		       .data_out1	(data_out1[3:0]));
+   probador probador (/*AUTOINST*/
+		      // Outputs
+		      .clk		(clk),
+		      .reset_L		(reset_L),
+		      .valid_in		(valid_in),
+		      .data_in		(data_in[3:0]),
+		      .valid_out	(valid_out[1:0]),
+		      // Inputs
+		      .data_out0	(data_out0[3:0]),
+		      .data_out1	(data_out1[3:0]));
 endmodule
--- Tarea6/demux_no_delay/src/probador.v
+++ Tarea6/demux_no_delay/src/probador.v
@@ -1,26 +1,40 @@
 module probador( //testbench: signal generator and data monitor
-    input [3:0]	data_out0, data_out1,			// From demux1_2 of demux.v
-    output reg clk,
-    output reg reset_L,			                // To demux1_2 of demux.v
-    output reg [3:0]	data_in);
+                    input [3:0]	data_out0, data_out1,			// From demux1_2 of demux.v
+                    output reg clk,
+                    output reg reset_L,			                // To demux1_2 of demux.v
+                    output reg valid_in,
+                    output reg [3:0] data_in,
+                    output reg valid_out0, valid_out1
+                    );
  
    initial
      begin
       $dumpfile("demux_1x2.vcd");               //Dumpfile to make in current folder 
       $dumpvars;                                //Directive to "dump" variables  
+         valid_out[0] <= 1;
+         valid_out[1] <= 1;
+
          reset_L <= 'b0;
          @(posedge clk);
          reset_L <= 1;                          //set reset to 1 to start the demux
-         
-         repeat(20)begin                        //run for first 10 cycles of the clock
+         valid_in <= 1;
+         repeat(10)begin                        //run for first 10 cycles of the clock
              @(posedge clk);
-             //data_in <= $random % 16;           //use $urandom or $random 
+                valid_out[0] <= 0;  
+         end
+         
+         repeat(10)begin                        //run for first 10 cycles of the clock
+             @(posedge clk);
+                valid_out[0] <= 1;  
          end
 
          repeat(10)begin
              @(posedge clk);
-             reset_L <= 0;
-             //data_in <= $random % 16;           //use $urandom or $random
+                valid_out[1] <= 0;   
+         end
+
+         repeat(5)begin
+          reset_L <= 0;
          end
          
          @(posedge clk);
@@ -31,5 +45,8 @@ module probador( //testbench: signal generator and data monitor
     always  #2 clk  <= ~clk;                    //"Toggle" each 2*10ns
     always  #10 data_in <= $random % 16;
     initial clk <= 0;
-    initial data_in <= 'b0;    
+    initial data_in <= 'b0;
+    //initial valid_in <= 'b0;
+    //initial valid_out <= 'b0;
+
 endmodule
